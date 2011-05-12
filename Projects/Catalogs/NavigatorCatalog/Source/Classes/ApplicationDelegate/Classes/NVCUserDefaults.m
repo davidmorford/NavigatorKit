f1181ad627e7395588332c309084129d285c7ddb
David Morford <david.morford@me.com>
2011-05-11 18:18:53 -0700

    Remove resources to relocate to simplified structure.

--- Projects/Catalogs/NavigatorCatalog/Source/Classes/ApplicationDelegate/Classes/NVCUserDefaults.m
+++
@@ -1,8 +0,0 @@
-
-#import "NVCUserDefaults.h"
-
-#pragma mark Application Preference Keys
-
-NSString * const NVCApplicationVersionKey		= @"NVCApplicationVersionKey";
-NSString * const NVCApplicationBuildNumberKey	= @"NVCApplicationBuildNumberKey";
-NSString * const NVCApplicationResetKey			= @"NVCApplicationResetKey";
